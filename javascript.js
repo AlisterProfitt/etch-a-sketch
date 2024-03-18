138b0d270d4001a8788fda3adf90db88a61d15ed
AlisterProfitt <alisterprofitt@gmail.com>
2024-03-18 17:04:06 -0400

    Add user-generated grid

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
 const biggie = document.querySelector('#biggie');
+const newGrid = document.querySelector('#newGrid');
+let grid;
 
 for (i = 0; i < 4; i++) {
     const row = document.createElement('div');
@@ -13,3 +15,32 @@ for (i = 0; i < 4; i++) {
     }
     biggie.appendChild(row);
 }
+
+function createNewGrid() {
+    while (biggie.firstChild) {
+        biggie.removeChild(biggie.firstChild);
+    }
+        for (i = 0; i < grid; i++) {
+        const row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        for (j = 0; j < grid; j++) {
+            const box = document.createElement('div');
+            box.setAttribute('class', 'box');
+            box.addEventListener('mouseenter', () => {
+                box.classList.add('hover')
+            })
+            row.appendChild(box);
+        }
+        biggie.appendChild(row);
+    }
+}
+
+newGrid.addEventListener('click', () => {
+    grid = prompt('How many squares per side do you want? Maximum is 100', '');
+        if (grid > 100) {
+            alert('Sorry! You have chosen a number that is too high!!!');
+        } else {
+            createNewGrid();
+        }
+})
+
