c592dc33b02a815d3600b230400e1a8cf1241261
AlisterProfitt <alisterprofitt@gmail.com>
2024-03-24 16:20:00 -0400

    update UI and add rainbow

--- javascript.js
+++ javascript.js
@@ -1,14 +1,32 @@
 const biggie = document.querySelector('#biggie');
 const newGrid = document.querySelector('#newGrid');
+const clearGrid = document.querySelector('#clearGrid')
+const newColorGrid = document.querySelector('#newColorGrid');
+const hexCharacters = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
 let grid;
 
-for (i = 0; i < 4; i++) {
+function getCharacter(index) {
+	return hexCharacters[index]
+}
+
+function generateNewColor() {
+	let hexColorRep = "#"
+
+	for (let index = 0; index < 6; index++){
+		const randomPosition = Math.floor ( Math.random() * hexCharacters.length ) 
+    	hexColorRep += getCharacter( randomPosition )
+	}
+	
+	return hexColorRep
+}
+
+for (i = 0; i < 16; i++) {
     const row = document.createElement('div');
     row.setAttribute('class', 'row');
-    for (j = 0; j < 4; j++) {
+    for (j = 0; j < 16; j++) {
         const box = document.createElement('div');
         box.setAttribute('class', 'box');
-        box.addEventListener('mouseenter', () => {
+        box.addEventListener('mouseover', () => {
             box.classList.add('hover')
         })
         row.appendChild(box);
@@ -26,7 +44,7 @@ function createNewGrid() {
         for (j = 0; j < grid; j++) {
             const box = document.createElement('div');
             box.setAttribute('class', 'box');
-            box.addEventListener('mouseenter', () => {
+            box.addEventListener('mouseover', () => {
                 box.classList.add('hover')
             })
             row.appendChild(box);
@@ -44,3 +62,39 @@ newGrid.addEventListener('click', () => {
         }
 })
 
+clearGrid.addEventListener('click', () => {
+    let nodelist = document.querySelectorAll('.box');
+    let boxes = Array.from(nodelist);
+    boxes.forEach((elem) => elem.classList.remove('hover'));
+    boxes.forEach((elem) => elem.style.backgroundColor = '');
+    
+})
+
+function createNewColorGrid() {
+    while (biggie.firstChild) {
+        biggie.removeChild(biggie.firstChild);
+    }
+        for (i = 0; i < grid; i++) {
+        const row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        for (j = 0; j < grid; j++) {
+            const box = document.createElement('div');
+            box.setAttribute('class', 'box');
+            box.addEventListener('mouseover', () => {
+            box.style.backgroundColor = generateNewColor();
+            console.log(generateNewColor())
+            })
+            row.appendChild(box);
+        }
+        biggie.appendChild(row);
+    }
+}
+
+newColorGrid.addEventListener('click', () => {
+    grid = prompt('How many squares per side do you want? Maximum is 100', '');
+        if (grid > 100) {
+            alert('Sorry! You have chosen a number that is too high!!!');
+        } else {
+            createNewColorGrid();
+        }
+})
